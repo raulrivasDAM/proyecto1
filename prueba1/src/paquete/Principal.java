63cc4b127861cb8b3ef837e066fe0020bdcef1b6
raulrivasDAM <dam_raulrivas@riberadeltajo.es>
2023-02-14 15:59:58 +0100

    Se agrega el principal al proyecto1

--- prueba1/src/paquete/Principal.java
+++ prueba1/src/paquete/Principal.java
@@ -3,7 +3,9 @@ package paquete;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+		System.out.println("El cambio a la clase principal");
+		
 
 	}
 
